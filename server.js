8d6d7c349a3dba31881f978da3b4ec783417eaaf
Vincent Liu <Vincent@Vincents-MBP.home>
2016-02-21 18:52:26 -0500

    updating server.js

--- server.js
+++ server.js
@@ -7,3 +7,37 @@ var express = require('express'),
     session = require('express-session'),
     app = express(),
     port = 3000 || process.env.PORT;
+
+
+// CONTROLLERS
+usersController = require('./controllers/usersController');
+fitCheckController = require('./controllers/fitCheckController');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ name: 'fit_check_auth_app', secret: 'hypebeast' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+app.use('/fitCheck', fitCheckController);
+app.use('/users', usersController);
+
+// REDIRECT ROUTE
+app.get('/', function(req, res){
+  res.redirect('/users');
+});
+
+// CONNECT & LISTEN
+mongoose.connect('mongodb://localhost:27017/fit_check');
+app.listen(port, function() {
+    console.log('LISTEN TO ME ON PORT ' + port);
+});
