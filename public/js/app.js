c9a942fa1c73b6918c2c29797004f7de8060ecb0
Vincent Liu <Vincent@Vincents-MBP.home>
2016-02-23 21:49:02 -0500

    adding animations

--- public/js/app.js
+++ public/js/app.js
@@ -23,4 +23,91 @@ window.onload = function() {
       imageReady: onUpload,
     });
   });
+
+  $('#newPost').click(function(){
+      $('#postContainer').addClass('animated fadeIn');
+      setTimeout(function () {
+        $('#postContainer').removeClass('animated fadeIn');
+      }, 1000);
+      $('#postContainer').css('visibility', 'visible');
+    });
+
+  $('#cancel').click(function(){
+    $('#postContainer').addClass('animated fadeOut');
+    setTimeout(function () {
+      $('#postContainer').removeClass('animated fadeOut');
+      $('#postContainer').css('visibility', 'hidden');
+      }, 1000);
+  });
+
+  $('#editPost').click(function(){
+    if($('#editPost').hasClass('off')){
+      $('.editMe').addClass('animated fadeIn');
+        setTimeout(function () {
+        $('.editMe').removeClass('animated fadeIn');
+        }, 1000);
+        $('.editMe').css('visibility', 'visible');
+        $('#editPost').removeClass('off')
+    } else {
+        $('.editMe').addClass('animated fadeOut');
+        setTimeout(function () {
+        $('.editMe').removeClass('animated fadeOut');
+        }, 1000);
+        $('#editPost').addClass('off')
+        $('.editMe').css('visibility', 'hidden');
+    }
+  });
+
+  $('#deletePost').click(function(){
+    if($('#deletePost').hasClass('off')){
+      $('.deleteMe').addClass('animated fadeIn');
+        setTimeout(function () {
+        $('.deleteMe').removeClass('animated fadeIn');
+        }, 1000);
+        $('.deleteMe').css('visibility', 'visible');
+        $('#deletePost').removeClass('off')
+    } else {
+        $('.deleteMe').addClass('animated fadeOut');
+        setTimeout(function () {
+        $('.deleteMe').removeClass('animated fadeOut');
+        }, 1000);
+        $('#deletePost').addClass('off')
+        $('.deleteMe').css('visibility', 'hidden');
+    }
+  });
+
+  $('#trending').hover(function(){
+    $('#trending').addClass('animated pulse');
+    setTimeout(function () {
+      $('#trending').removeClass('animated pulse');
+      }, 1000);
+  });
+
+  $('#logmeout').hover(function(){
+    $('#logmeout').addClass('animated pulse');
+    setTimeout(function () {
+      $('#logmeout').removeClass('animated pulse');
+      }, 1000);
+  });
+
+  $('#newPost').hover(function(){
+    $('#newPost').addClass('animated pulse');
+    setTimeout(function () {
+      $('#newPost').removeClass('animated pulse');
+      }, 1000);
+  });
+
+  $('#editPost').hover(function(){
+    $('#editPost').addClass('animated pulse');
+    setTimeout(function () {
+      $('#editPost').removeClass('animated pulse');
+      }, 1000);
+  });
+
+  $('#deletePost').hover(function(){
+    $('#deletePost').addClass('animated pulse');
+    setTimeout(function () {
+      $('#deletePost').removeClass('animated pulse');
+      }, 1000);
+  });
 };
